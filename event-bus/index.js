8b7b24285a31027f0efa171f5d3bc5a544daf2f2
NeoGlanding <neoglanding@protonmail.com>
2022-02-20 20:16:19 +0700

    auto commit without message

--- event-bus/index.js
+++ event-bus/index.js
@@ -19,7 +19,7 @@ app.post('/events', async (req, res) => {
     try {
         await axios.post('http://localhost:4000/events', event)
         await axios.post('http://localhost:4001/events', event)
-        // await axios.post('http://localhost:4002/events', event)
+        await axios.post('http://localhost:4002/events', event)
         await axios.post('http://localhost:4003/events', event)
 
         res.send({
